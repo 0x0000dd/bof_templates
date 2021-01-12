d0f2315ec4dc00cc77dac46295f42de8440bae25
0x0000dd <65570372+0x0000dd@users.noreply.github.com>
2021-01-12 08:58:35 -0600

    Add files via upload

---
+++ 1-fuzz.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+
+import sys, socket
+from time import sleep
+
+rhost = "0.0.0.0"
+rport = 1234
+
+junk = "A" * 100
+
+while True:
+	try:
+		s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+		s.connect((rhost,rport))
+		print "Sending: %s" % str(len(junk))
+		s.send(junk)
+		s.close()
+		sleep(3)
+		junk += "A" * 100
+	except:
+		print "Crashed around %s bytes" % str(len(junk))
+		sys.exit()
